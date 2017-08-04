d9514f455486c7b6f65d687d43189b1a59a40aba
AndrewKiri <andrewkirichok@gmail.com>
2017-08-04 21:45:48 +0200

    adding changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 var gulp = require('gulp');
 var webpack = require('gulp-webpack');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function () {
+gulp.task('default', ['serve']);
+
+gulp.task('webpack', function() {
 	return gulp.src('app_src/js/main.js')
 		.pipe(webpack(require('./webpack.config.js')))
 		.pipe(gulp.dest('app_src/js/'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['webpack'], function(){
+	browserSync.init({
+		server: './app_src'
+	});
+	gulp.watch('./app_src/js/components/*.js', ['webpack']);
+	gulp.watch('./app_src/index.html').on('change', browserSync.reload);
 });
